2118c04af2a00a429e022c4bba97d0cb4da9fc7c
Arran France <arranf@gmail.com>
2017-08-22 09:58:46 +0100

    Split job for Connection Requests into an alert and a reminder.

--- Jobs/ConnectionRequestAlert.cs
+++ Jobs/ConnectionRequestAlert.cs
@@ -12,15 +12,21 @@ using Rock.Model;
 
 namespace com.bricksandmortarstudio.TheCrossing.Jobs
 {
-    [SystemEmailField( "Email", "The System Email to send for each person with open connection requests" )]
-    [IntegerField( "Exclude Hours", "Only connection requests older than this many hours will be sent. This prevents new requests being sent as a reminder.", true, 18 )]
-    public class ConnectionRequestReminder : IJob
+    [SystemEmailField( "Email", "The System Email to send for each person with new connection requests" )]
+    [IntegerField( "Include Hours", "Only connection requests created within the past this many hours will be sent if we cannot determine when the last job was run. This ensures only new requests are sent as a reminder.", true, 18 )]
+    public class ConnectionRequestAlert : IJob
     {
-        public ConnectionRequestReminder() { }
+        public ConnectionRequestAlert() { }
         public void Execute( IJobExecutionContext context )
         {
             var rockContext = new RockContext();
             var dataMap = context.JobDetail.JobDataMap;
+            DateTime? lastRun = null;
+            if ( context.PreviousFireTimeUtc.HasValue )
+            {
+                lastRun = RockDateTime.ConvertLocalDateTimeToRockDateTime( context.PreviousFireTimeUtc.Value.LocalDateTime );
+            }
+
             var systemEmailGuid = dataMap.GetString( "Email" ).AsGuidOrNull();
 
             string appRoot = Rock.Web.Cache.GlobalAttributesCache.Read().GetValue( "ExternalApplicationRoot" );
@@ -35,32 +41,22 @@ namespace com.bricksandmortarstudio.TheCrossing.Jobs
                 throw new Exception( "The system email template setting is not a valid system email template." );
             }
 
-            var cutOffHours = dataMap.GetString( "ExcludeHours" ).AsIntegerOrNull();
+            var cutOffHours = dataMap.GetString( "IncludeHours" ).AsIntegerOrNull();
             if ( !cutOffHours.HasValue )
             {
                 throw new Exception( "A cutoff period needs to be set." );
             }
 
-
-
-
-            var cutoff = RockDateTime.Now.AddHours( -1 * cutOffHours.Value );
-
+            if ( lastRun == null )
+            {
+                lastRun = RockDateTime.Now.AddHours( -1 * cutOffHours.Value );
+            }
 
             var connectionRequestService = new ConnectionRequestService( rockContext );
-            var midnightToday = new DateTime( RockDateTime.Now.Year, RockDateTime.Now.Month, RockDateTime.Now.Day );
-            var currentDateTime = RockDateTime.Now;
             var openConnectionRequests =
                 connectionRequestService.Queryable()
                                         .AsNoTracking()
-                                        .Where( cr => cr.CreatedDateTime >= cutoff && ( cr.ConnectionState == ConnectionState.Active || ( cr.ConnectionState == ConnectionState.FutureFollowUp && cr.FollowupDate.HasValue && cr.FollowupDate.Value < midnightToday ) ) );
-
-            int totalCriticalCount = openConnectionRequests.Count( cr => cr.ConnectionStatus.IsCritical );
-            int totalIdleCount = openConnectionRequests
-                                        .Count( cr =>
-                                            ( cr.ConnectionRequestActivities.Any() && cr.ConnectionRequestActivities.Max( ra => ra.CreatedDateTime ) < SqlFunctions.DateAdd( "day", -cr.ConnectionOpportunity.ConnectionType.DaysUntilRequestIdle, currentDateTime ) )
-                                            || ( !cr.ConnectionRequestActivities.Any() && cr.CreatedDateTime < SqlFunctions.DateAdd( "day", -cr.ConnectionOpportunity.ConnectionType.DaysUntilRequestIdle, currentDateTime ) )
-                                        );
+                                        .Where( cr => cr.CreatedDateTime >= lastRun && cr.ConnectionState != ConnectionState.Connected );
 
             var groupedRequests = openConnectionRequests
                 .ToList()
@@ -74,12 +70,7 @@ namespace com.bricksandmortarstudio.TheCrossing.Jobs
                 var mergeFields = new Dictionary<string, object>
                             {
                                 {"ConnectionRequests", connectionRequests},
-                                {"Person", connectionRequestGrouping.Key.Person},
-                                {"CriticalCount", connectionRequests.Count(cr => cr.ConnectionStatus.IsCritical) },
-                                {"IdleCount", connectionRequests.Count(cr => ( cr.ConnectionRequestActivities.Any() && cr.ConnectionRequestActivities.Max( ra => ra.CreatedDateTime ) < SqlFunctions.DateAdd( "day", -cr.ConnectionOpportunity.ConnectionType.DaysUntilRequestIdle, currentDateTime ) )
-                                            || ( !cr.ConnectionRequestActivities.Any() && cr.CreatedDateTime < SqlFunctions.DateAdd( "day", -cr.ConnectionOpportunity.ConnectionType.DaysUntilRequestIdle, currentDateTime ) )) },
-                                {"TotalIdleCount", totalIdleCount },
-                                {"TotalCriticalCount", totalCriticalCount }
+                                {"Person", connectionRequestGrouping.Key.Person}
                             };
 
                 var recipients = new List<string> { connectionRequestGrouping.Key.Person.Email };
